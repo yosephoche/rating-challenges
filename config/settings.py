eefb4917fa20380c09a5b85d38687ad98b2da7c7
Yoseph <yosephoche@gmail.com>
2022-02-08 00:05:58 +0800

    handle cors error

--- config/settings.py
+++ config/settings.py
@@ -37,10 +37,16 @@ DJANGO_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
 ]
 
 APPS = [
     'core'
+]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + APPS
@@ -49,6 +55,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
